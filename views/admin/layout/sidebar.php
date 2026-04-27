2f0399418589567f721566ed6823ba1dc18e5fb7
Rebeh Ben Aoun <benaounrebeh@gmail.com>
2026-04-27 20:45:20 +0100

    Create sidebar.php

---
+++ views/admin/layout/sidebar.php
@@ -0,0 +1,14 @@
+<div class="container">
+
+<div class="sidebar">
+    <h3>Admin</h3>
+    <a href="?page=admin.dashboard">Dashboard</a><br>
+    <a href="?page=admin.semesters">Semesters</a><br>
+    <a href="?page=admin.courses">Courses</a><br>
+    <a href="?page=admin.professors">Professors</a><br>
+    <a href="?page=admin.students">Students</a><br>
+    <a href="?page=admin.enrollments">Enrollments</a><br>
+    <a href="?page=admin.assignments">Assignments</a><br>
+</div>
+
+<div class="content">
