8adf408526f1cdacbd85c6ee9522305844f390cb
Rebeh Ben Aoun <benaounrebeh@gmail.com>
2026-04-27 21:02:50 +0100

    Add form to manage professors in admin view

--- views/admin/professors.php
+++ views/admin/professors.php
@@ -1,1 +1,32 @@
+<?php include 'layout/header.php'; ?>
+<?php include 'layout/sidebar.php'; ?>
 
+<h2>Professors</h2>
+
+<form method="POST" action="?page=admin.saveProfessor">
+
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Password">
+
+    <button class="btn btn-primary">Save</button>
+</form>
+
+<hr>
+
+<table>
+<tr>
+    <th>Name</th>
+    <th>Email</th>
+</tr>
+
+<?php foreach ($professors ?? [] as $p): ?>
+<tr>
+    <td><?= htmlspecialchars($p['name']) ?></td>
+    <td><?= htmlspecialchars($p['email']) ?></td>
+</tr>
+<?php endforeach; ?>
+
+</table>
+
+<?php include 'layout/footer.php'; ?>
